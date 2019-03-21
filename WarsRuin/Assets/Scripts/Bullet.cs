01ec5767ba8c285ec6c453726bde3ee5df6457f2
Raiyana Haque <haque.raiyana@outlook.com>
2019-03-21 11:23:37 +0000

    All stuff done in test level

---
+++ WarsRuin/Assets/Scripts/Bullet.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float bulletSpeed = 125f;
+    public float lifeDuration = 1f;
+
+    private float lifeTime;
+
+    public float nextBullet = 1f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lifeTime = lifeDuration;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * bulletSpeed * Time.deltaTime;
+
+        lifeTime -= Time.deltaTime;
+        if(lifeTime <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            nextBullet = Time.time + bulletSpeed;
+        }
+    }
+}
---
+++ WarsRuin/Assets/Scripts/Gun.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            GameObject bulletObject = Instantiate(bulletPrefab);
+            bulletObject.transform.position = this.transform.position + this.transform.forward;
+            bulletObject.transform.forward = this.transform.forward;
+
+        }
+    }
+}
