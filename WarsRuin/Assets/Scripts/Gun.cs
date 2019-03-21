49b44b7a5cdcfc4b2786c658dc969475952d11f5
Raiyana Haque <haque.raiyana@outlook.com>
2019-03-21 09:58:16 +0000

    Instantiating bullets

---
+++ WarsRuin/Assets/Scripts/Gun.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            GameObject bulletObject = Instantiate(bulletPrefab);
+            bulletObject.transform.position = this.transform.position;
+        }
+    }
+}
