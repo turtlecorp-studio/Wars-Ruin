0b1768d474ece4ceb2c905a6620af2c019713f6f
Raiyana Haque <haque.raiyana@outlook.com>
2019-03-22 11:54:06 +0000

    Character

--- WarsRuin/Assets/Scripts/ItemPickup.cs
+++ WarsRuin/Assets/Scripts/ItemPickup.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class ItemPickup : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Inventory inventory;
+
+    private void Start()
     {
-        
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player"))
+        {
+            for (int i = 0; i < inventory.slots.Length; i++)
+            {
+                if(inventory.isFull[i] == false)
+                {
+                    inventory.isFull[i] = true;
+                    break;
+                } //507
+
+            }
+        }
     }
 }
