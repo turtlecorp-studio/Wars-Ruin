8e4af4c7358a6ea57e314c95ea5a9f974b2d77f9
Raiyana Haque <haque.raiyana@outlook.com>
2019-04-25 10:44:21 +0100

    PostProcessing

---
+++ WarsRuin/Assets/Scripts/ThoughtTrigger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThoughtTrigger : MonoBehaviour
+{
+    public GameObject playerThoughtPanel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerThoughtPanel.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+     void OnTriggerEnter(Collider other)
+    {
+        other.gameObject.tag = "Player";
+            Destroy(playerThoughtPanel);
+            
+    }
+}
