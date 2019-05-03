214b0bcbf51db04b82e08282ab0092a6e9b96734
Raiyana Haque <haque.raiyana@outlook.com>
2019-05-03 16:07:40 +0100

    Korra is better

--- WarsRuin/Assets/Scripts/TestDialog.cs
+++ WarsRuin/Assets/Scripts/TestDialog.cs
@@ -4,40 +4,43 @@ using UnityEngine;
 
 public class TestDialog : MonoBehaviour
 {
+    //public GameObject dialoguePanel;
     public DialogueBase dialogue;
     public GameObject alertIcon;
     public GameObject lightIndicator;
     public AudioSource triggerSound;
 
+    void Start()
+    {
+        //dialoguePanel = GameObject.FindGameObjectWithTag("DialoguePanel");
+    }
     public void TriggerDialogue()
     {
         DialogueManager.instance.EnqueueDialogue(dialogue);
     }
 
-    /* private void Update()
-     {
-
-         if(Input.GetKeyDown(KeyCode.Space))
-         {
-             TriggerDialogue();
-         }
-     } */
-
+    
+         
     void OnTriggerStay(Collider other)
     {
         if (Input.GetKeyDown("space") && other.tag == "Player")
         {
             Cursor.visible = true;
             Cursor.lockState = CursorLockMode.None;
-            GameObject.Find("Camera").GetComponent<ThirdPersonCamera>().enabled = false;
+            //GameObject.Find("Camera").GetComponent<ThirdPersonCamera>().enabled = false;
+           // GameObject.Find("Player").GetComponent<PlayerController>().enabled = false;
             triggerSound.Play();
             TriggerDialogue();
             Destroy(alertIcon);
-           // Destroy(gameObject);
-            //Destroy(lightIndicator);
-        }
+
+        } /*else if (GameObject.Find("DialoguePanel").activeSelf == false)
+        {
+            GameObject.Find("Camera").GetComponent<ThirdPersonCamera>().enabled = true;
+            GameObject.Find("Player").GetComponent<PlayerController>().enabled = true;
+        } */
     }
 
+    
 
     void OnTriggerExit(Collider other)
     {
@@ -45,7 +48,6 @@ public class TestDialog : MonoBehaviour
         {
             Destroy(gameObject);
             Destroy(lightIndicator);
-            GameObject.Find("Camera").GetComponent<ThirdPersonCamera>().enabled = true;
         }
     }
 
