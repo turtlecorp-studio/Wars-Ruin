84b090eca36ec69761203f1f652b0a00c0ef0660
Raiyana Haque <haque.raiyana@outlook.com>
2019-05-16 12:51:24 +0100

    BattleMap

--- WarsRuin/Assets/Scripts/TextTypewriter.cs
+++ WarsRuin/Assets/Scripts/TextTypewriter.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TextTypewriter : MonoBehaviour
 {
+    public float delay = 0.1f;
+    [TextArea]public string fullText;
+    private string _currentText = "";
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(ShowText());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator ShowText ()
     {
-        
+        var textComponent = this.GetComponent<Text>();
+        textComponent.text = "";
+        for (int i = 0; i < fullText.Length; i++)
+        {
+            _currentText = fullText.Substring(0, i);
+            textComponent.text = _currentText;
+            yield return new WaitForSeconds(delay);
+        }
     }
 }
