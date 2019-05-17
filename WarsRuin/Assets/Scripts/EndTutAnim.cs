5b88bca3e6fd5dd4b5d826d46693e926493e77d1
Raiyana Haque <haque.raiyana@outlook.com>
2019-05-17 10:55:43 +0100

    UI

---
+++ WarsRuin/Assets/Scripts/EndTutAnim.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTutAnim : MonoBehaviour
+{
+    public Animation tutorialAnimation;
+    public GameObject tutorialCard;
+    public GameObject tutorialStarter;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            tutorialCard.SetActive(true);
+            tutorialAnimation = tutorialCard.GetComponent<Animation>();
+            tutorialAnimation.Play("EndTutorialCard");
+            tutorialAnimation.Play("PlayerThoughtsOut");
+            Destroy(tutorialStarter);
+            Destroy(this.gameObject, 0.5f);
+        }
+    }
+}
---
+++ WarsRuin/Assets/Scripts/PlayTutAnim.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayTutAnim : MonoBehaviour
+{
+    public Animation tutorialAnimation;
+    public GameObject tutorialCard;
+
+    private void Start()
+    {
+        tutorialCard.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            tutorialCard.SetActive(true);
+            tutorialAnimation = tutorialCard.GetComponent<Animation>();
+            tutorialAnimation.Play("PlayerThoughtsIn");
+            tutorialAnimation.Play("TutorialCard");
+        }
+    }
+}
